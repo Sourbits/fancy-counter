7bebd42815cb8747a948fa1b7dff8f0f8e50ffaa
Sourbits <a36947325@gmail.com>
2023-12-05 19:03:08 -0800

    Added buttons to add or decrease and reset counter

---
+++ script.js
@@ -0,0 +1,27 @@
+const increaseButtonE1 = document.querySelector(".counter__button--increase");
+const decreaseButtonE1 = document.querySelector(".counter__button--decrease");
+const resetButtonE1 = document.querySelector(".counter__reset-button");
+const counterValueE1 = document.querySelector(".counter__value");
+
+function incrementCounter() {
+  const currentValue = counterValueE1.textContent;
+  const currentValueAsNumber = +currentValue;
+  const newValue = currentValueAsNumber + 1;
+  counterValueE1.textContent = newValue;
+}
+
+resetButtonE1.addEventListener("click", function () {
+  counterValueE1.textContent = 0;
+});
+
+increaseButtonE1.addEventListener("click", incrementCounter);
+
+decreaseButtonE1.addEventListener("click", function () {
+  const currentValue = counterValueE1.textContent;
+  const currentValueAsNumber = +currentValue;
+  let newValue = currentValueAsNumber - 1;
+
+  counterValueE1.textContent = newValue;
+});
+
+document.addEventListener("keydown", incrementCounter);
